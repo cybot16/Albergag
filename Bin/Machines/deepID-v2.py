bd3a7fcf4619393dd5303ab377dd4bd00c15fc12
cybot16 <abdellah.sabry@gmail.com>
2018-02-21 14:38:07 +0000

    Update deepID-v2.py

--- Bin/Machines/deepID-v2.py
+++ Bin/Machines/deepID-v2.py
@@ -17,12 +17,12 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 # tf.logging.set_verbosity(tf.logging.INFO)
 
 # Convolutional neural net contrusction function
- # It describes every layer separetly
+# It describes every layer separetly
  
 def cnn_model_fn(features, labels, mode):
     # Input Layer, standard square 31*31 gray 
     input_layer = tf.reshape(features, [-1, 31, 31, 1])
-    
+ 
     # Convolutional Layer #1, takes as the input the input_layer, valid padding and the AF is a ReLU
     conv1 = tf.layers.conv2d(
         inputs=input_layer,
@@ -30,8 +30,7 @@ def cnn_model_fn(features, labels, mode):
         kernel_size=[4, 4],
         padding="valid",
         activation=tf.nn.relu
-    )
-     
+    )     
     # Normalization Layer #1, experimental normalization layer just after the conv layer
     # norm1 = tf.layers.local_response_normalization(
     #   inputs=conv1,
@@ -39,7 +38,7 @@ def cnn_model_fn(features, labels, mode):
     #   alpha=0.0001,
     #   beta=0.75
     #)
-
+ 
 
     # Pooling Layer #1, using maxplooling takes as an input the conv1 layer
     pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
@@ -60,12 +59,11 @@ def cnn_model_fn(features, labels, mode):
     #   alpha=0.0001,
     #   beta=0.75
     #)
-
-    # Pooling Layer #2
-  
+ 
+    # Pooling Layer #2, takes as an input the output of the 2nd convonlutional layer 
     pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
 
-# Convolutional Layer #3
+    # Convolutional Layer #3, takes as the input the output of the 2nd max_pooling layer, augumenting the dimensions to 60
     conv3 = tf.layers.conv2d(
         inputs=pool2,
         filters=60,
@@ -73,14 +71,11 @@ def cnn_model_fn(features, labels, mode):
         padding='valid',
         activation=tf.nn.relu
     )
-   
-  
-# Pooling Layer #3
-  
+    
+    # Pooling Layer #3, finaly pooling layer
     pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[2, 2], strides=2)
 
-# Convolutional Layer #4
-
+    # Convolutional Layer #4, final convolution layer, the output has 80 in depth
     conv4 = tf.layers.conv2d(
         inputs=pool3,
         filters=80,
@@ -89,38 +84,38 @@ def cnn_model_fn(features, labels, mode):
         activation=tf.nn.relu
     )
  
-# Flatten tensor into a batch of vectors
-# Input Tensor Shape: [batch_size, 1, 2, 80]
-# Output Tensor Shape: [batch_size, 1 * 2 * 80]
+    # Flatten tensor into a batch of vectors
+    # Input Tensor Shape: [batch_size, 1, 2, 80]
+    # Output Tensor Shape: [batch_size, 1 * 2 * 80]
     conv4_flat = tf.reshape(conv4, [-1, 80])
     pool3_flat = tf.reshape(pool3, [-1, 80])
-
-# Concat conv4 and pool3
- #   con = tf.concat(['conv4_flat','pool3_flat'], axis = -1)
-# Dense Layer
-# Densely connected layer with 1024 neurons
-# Input Tensor Shape: [batch_size, 1 * 2 * 80]
-# Output Tensor Shape: [batch_size, 1024]
-
+    
+    # Experimental
+    
+    # Concat conv4 and pool3
+    # conc = tf.concat(['conv4_flat','pool3_flat'], axis = -1)
+    
+    # Dense Layer
+    # Densely connected layer with 1024 neurons
+    # Input Tensor Shape: [batch_size, 1 * 2 * 80]
+    # Output Tensor Shape: [batch_size, 1024]
     dense = tf.layers.dense(inputs=conv4_flat, units=1024, activation=tf.nn.relu)
 
-# Add dropout operation; 0.6 probability that element will be kept
-
+    # Add dropout operation; 0.6 probability that the element will be kept
     dropout = tf.layers.dropout(
         inputs=dense,
         rate=0.6,
         training=mode == learn.ModeKeys.TRAIN
     )
 
-# Logits layer
-# Input Tensor Shape: [batch_size, 1024]
-# Output Tensor Shape: [batch_size, 10]
+    # Logits layer
+    # Input Tensor Shape: [batch_size, 1024]
+    # Output Tensor Shape: [batch_size, 10]
     logits = tf.layers.dense(inputs=dropout, units=10)
-
     loss = None
     train_op = None
 
-# Calculate Loss (for both TRAIN and EVAL modes)
+    # Calculate Loss (for both TRAIN and EVAL modes)
 
     if mode != learn.ModeKeys.INFER:
         onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=10)
